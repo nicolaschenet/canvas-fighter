c73761ce34bb9b2ccb2d7b6ed5d16511b48d1c02
Nicolas Chenet <ironic51@gmail.com>
2012-10-18 01:18:42 +0200

    Big animator rewrite: it's prettier now ;)

--- README.md
+++ README.md
@@ -33,4 +33,14 @@ Super light start, you really shouldn't take a look at it right now.
  - Only one character, buggy sprite, resulting in buggy animation.
  - The stage and the fighter are rendered in canvas, that's a nice start \o/
 
-#### More to come...
+#### 2012/10/18
+
+ - Fighter and Animator classes are now separated from each other.
+ - Animations are based on keyframes: it's easier to compose the animation from the sprite (repeat some frames, go back and forth...).
+ - Animations can be repeated forever.
+ - Animations can be chained: once an animation is finished, the callback animation is played, and so on.
+ - Animator loop has lost some weight \o/
+
+#### Next step...
+
+Yeahhh the sprite is still buggy... I have to recompose the sprite so that each frame has the same width and height. Will be the next step, with the creation of all animations (keyframes sequences).
--- canvas-fighter.js
+++ canvas-fighter.js
@@ -1,23 +1,23 @@
 $(function () {
 
-	var CanvasFighter = function () {
+    var CanvasFighter = function () {
 
         // Default options
 
-        var   width         = 800
-            , height        = 600
-            , floorY        = 50
-            , c             = document.getElementById("stage")
-            , ctx           = c.getContext("2d")
+        var   width    = 800
+            , height   = 600
+            , floorY   = 50
+            , c        = document.getElementById("stage")
+            , ctx      = c.getContext("2d")
             , gLoop
-            , self          = this
+            , self     = this
         ;
 
 
         // Let's set canvas dimensions
 
-        c.width     = width;
-        c.height    = height;
+        c.width  = width;
+        c.height = height;
 
 
         // Clearing of the canvas, will be called on each frame
@@ -30,153 +30,11 @@ $(function () {
             ctx.fill();
         };
 
-        var Fighter = function () {
-
-            var   sprite        = new Image()
-                , self          = this
-            ;
-
-            // ATM, the only fighter provided is Ryu
-
-            // This sprite has to be redesigned in order to be efficient
-            sprite.src          = "sprites/Ryu.gif";
-
-            self.name           = "Ryu";
-            self.width          = 76;
-            self.height         = 120;
-
-            self.x              = 0;
-            self.y              = 0;
-
-            self.state          = "idle";
-
-            var Animator = function () {
-
-                this.speed             = 3;
-                this.frameCounter      = 0;
-                this.currentFrame      = 0;
-                this.currentAnimation  = "";
-
-                this.resetState = function () {
-                    this.frameCounter = this.currentFrame = 0;
-                }
-
-                this.drawFrame = function (frame) {
-                    ctx.drawImage (
-                        sprite,
-                        frame.x,
-                        frame.y,
-                        self.width,
-                        self.height,
-                        self.x,
-                        self.y,
-                        self.width,
-                        self.height
-                    );
-                }
-
-                this.animations = {
-
-                    "entering-stage"  : {
-
-                        keyframes  : [
-                              { x : self.width * 0 +10, y : 0 }
-                            , { x : self.width * 1 +10, y : 0 }
-                            , { x : self.width * 2 +10, y : 0 }
-                            , { x : self.width * 3 +10, y : 0 }
-                            , { x : self.width * 4 +10, y : 0 }
-                            , { x : self.width * 5 +10, y : 0 }
-                            , { x : self.width * 6 +10, y : 0 }
-                        ]
-
-                        , play  : function () {
-                            console.log(self.animator.currentFrame);
-                            // This animation is played only once, before the beginning of the fight
-
-                            if ( self.animator.currentFrame < this.keyframes.length ) {
-
-                                // animation is running, go on Baby
-                                self.animator.drawFrame(this.keyframes[self.animator.currentFrame]);
-
-                                if ( self.animator.frameCounter == self.animator.speed ) {
-                                    self.animator.frameCounter = 0;
-                                    self.animator.currentFrame++;
-
-                                } else {
-                                    self.animator.frameCounter++;
-                                }
-
-                            } else {
-                                // Ryu has finished his pre-fight dance, get ready to fight, Challenger !
-                                self.setState("ready-to-fight");
-                            }
-                        }
-                    }
-
-                    , "ready-to-fight"  : {
-
-                        keyframes  : [
-                        ]
-
-                        , play  : function () {
-                            ctx.drawImage(sprite, 32, self.height - 6, self.width, self.height, self.x, self.y, self.width, self.height);
-                        }
-                    }
-
-                    , "jumping" : {
-
-                    }
-
-                    , "falling" : {
-
-                    }
-
-                    , "walking" : {
-
-                    }
-
-                    , "hadoken" : {
-
-                    }
-
-                    , "shoryuken" : {
-
-                    }
-                }
-            }
-
-            self.animator = new Animator();
-
-            self.loopAnimation = function () {
-                self.animator.animations[self.state].play();
-            }
-
-
-            self.setState = function (state) {
-                self.state = state;
-
-                self.animator.resetState();
-                self.animator.animations[self.state].play(); // Initial play to avoid blank states
-
-            }
-
-            self.setPosition = function ( x, y ) {
-                self.x = x;
-                self.y = y;
-            }
-
-        };
-
-        var fighter = new Fighter();
-
-        fighter.setPosition( 100, height - fighter.height - floorY );
-        fighter.setState("entering-stage");
-
 
         var GameLoop = function () {
             clear();
-            fighter.loopAnimation();
-            gLoop = setTimeout(GameLoop, 1000 / 60);  // Global animation speed is set here
+            animator.loop();
+            gLoop = setTimeout(GameLoop, 1000 / 60);
         };
 
 
@@ -187,7 +45,169 @@ $(function () {
             })
         }
 
-	}
+
+        /** Fighter Class ****************************/
+
+        // ATM, the only fighter is Ryu
+
+        var Fighter = function () {
+
+            var fighter = this;
+
+            fighter.sprite        = new Image();
+            fighter.sprite.src    = "sprites/Ryu.gif";              // This sprite has to be redesigned in order to be efficient
+
+            fighter.name          = "Ryu";
+            fighter.width         = 76;
+            fighter.height        = 120;
+
+            fighter.x             = 0;
+            fighter.y             = 0;
+
+            fighter.state         = "entering-stage";              // Default state / Associated animation should always be "repeat"
+
+            fighter.animations    = {
+
+                // The names of animations are associated
+                // with the fighter's states
+
+                "entering-stage"  : {
+
+                    keyframes  : [
+                          { x : fighter.width * 0 +10, y : 0 }
+                        , { x : fighter.width * 1 +10, y : 0 }
+                        , { x : fighter.width * 2 +10, y : 0 }
+                        , { x : fighter.width * 3 +10, y : 0 }
+                        , { x : fighter.width * 4 +10, y : 0 }
+                        , { x : fighter.width * 5 +10, y : 0 }
+                        , { x : fighter.width * 6 +10, y : 0 }
+                    ]
+                    , repeat      : false
+                    , callback    : "ready-to-fight"
+
+                }
+
+                , "ready-to-fight"  : {
+
+                    keyframes  : [
+                        { x : 32, y : fighter.height - 6 }
+                    ]
+                    , repeat: true
+
+                }
+
+                , "standing" : {
+
+                }
+
+                , "crouching" : {
+
+                }
+
+                , "jumping" : {
+
+                }
+
+                , "falling" : {
+
+                }
+
+                , "walking" : {
+
+                }
+
+                , "hadoken" : {
+
+                }
+
+                , "shoryuken" : {
+
+                }
+            };
+
+
+            fighter.setState = function (state) {
+                fighter.state = state;
+            }
+
+            fighter.setPosition = function ( x, y ) {
+                fighter.x = x;
+                fighter.y = y;
+            }
+
+        };
+
+
+        /** Animator Class ******************************/
+
+        // Animates the fighter depending on its state...
+
+        var Animator = function (fighter) {
+
+            var animator = this;
+
+            animator.speed             = 3;
+            animator.frameCounter      = 0;
+            animator.currentFrame      = 0;
+
+            animator.resetFrames = function () {
+                this.frameCounter = this.currentFrame = 0;
+            }
+
+            animator.drawFrame = function (frame) {
+                ctx.drawImage (
+                    fighter.sprite,
+                    frame.x,
+                    frame.y,
+                    fighter.width,
+                    fighter.height,
+                    fighter.x,
+                    fighter.y,
+                    fighter.width,
+                    fighter.height
+                );
+            }
+
+            animator.play = function (animation) {
+
+                animator.drawFrame(animation.keyframes[animator.currentFrame]);
+
+                if ( animator.frameCounter == animator.speed ) {
+
+                    animator.frameCounter = 0;
+                    animator.currentFrame++;
+
+                } else {
+
+                    animator.frameCounter++;
+
+                }
+
+            }
+
+            animator.loop = function () {
+
+                if ( animator.currentFrame >= fighter.animations[fighter.state].keyframes.length ) {
+                    // Animation is finish, callback please !!
+                    fighter.setState(fighter.animations[fighter.state].callback);
+                    animator.resetFrames();
+
+                } else if (fighter.animations[fighter.state].repeat) {
+                    animator.resetFrames();
+                }
+
+                animator.play(fighter.animations[fighter.state]);
+
+            }
+
+        }
+
+        var ryu         = new Fighter();
+        var animator    = new Animator(ryu);
+
+        ryu.setPosition( 100, height - ryu.height - floorY );
+
+    }
 
 
     // Let's init our CanvasFighter instance
